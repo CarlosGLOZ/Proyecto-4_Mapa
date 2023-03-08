0dbf99d8f3c32bc8824055cc876f174c1444d664
Ricardo <100007001.joan23@fje.edu>
2023-03-08 20:03:11 +0100

    Login/Registrar acabado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,16 +8,18 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" id="_token">
     <title>Mapa</title>
     <script src="https://kit.fontawesome.com/2b5286e1aa.js" crossorigin="anonymous"></script>
-    <link href="{{asset('css/app.css')}}" rel="stylesheet">  
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
     @stack('css')
     @stack('js')
 
-    @yield('head')
+    @stack('head')
 </head>
+
 <body>
     {{-- Navbar --}}
 
     {{-- Contenido --}}
     @yield('content')
 </body>
+
 </html>
