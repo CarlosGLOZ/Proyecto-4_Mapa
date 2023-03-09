29ef63f235dbcb5e56231c8a942cb95c9ca42324
Ricardo <100007001.joan23@fje.edu>
2023-03-09 17:47:28 +0100

    Inicio GincanaPlay

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\GincanaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,3 +22,8 @@ Route::get('/', [Controller::class, 'index'])->name('home');
 Route::get('/auth/LoginRegistrar', [AuthController::class, 'showLoginRegistrar'])->name('auth.LoginRegistrar');
 Route::post('/auth/registrar', [AuthController::class, 'registrar']);
 Route::post('/auth/login', [AuthController::class, 'login']);
+
+
+
+//GincanaPlay
+Route::get('/gincana/GincanaPlay', [GincanaController::class, 'showGincanaPlay'])->name('gincana.GincanaPlay');
