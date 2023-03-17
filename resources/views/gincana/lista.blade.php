b44374cb52912374bb44a4d8fdb28d0a772ef33a
CarlosGLOZ <100007082.joan23@fje.edu>
2023-03-17 18:23:11 +0100

    filtros gincanas a medias

--- resources/views/gincana/lista.blade.php
+++ resources/views/gincana/lista.blade.php
@@ -26,8 +26,8 @@
             <div id="menu-principal-header">GYMKHANAS</div>
             <form action="{{ route('gincana.filtrar') }}" method="POST" id="menu-gincanas-filtros">
                 @csrf
-                <input type="text" name="buscar" id="menu-gincanas-filtro-nombre">
-                <i class="fa-regular fa-user" id="gincana-propia-toggle"></i>
+                <input type="text" name="buscar" id="menu-gincanas-filtro-nombre" placeholder="Buscar...">
+                <button class="boton-toggle-inactivo" id="gincana-propia-toggle" data-activo="false">Mis Gincanas</button>
             </form>
             <div id="menu-gincanas" class="menu-principal-botones">
                 <button class="boton-menu-principal" id="menu-principal-boton-atras"><i class="fa-solid fa-chevron-left"></i><a href="{{ route('home') }}">Atrás</a></button>
