d0448fe4a7402df8fd629235c4bd8c89ae8286b0
CarlosGLOZ <100007082.joan23@fje.edu>
2023-03-17 17:26:58 +0100

    urls gincanas

--- app/Http/Controllers/GincanaController.php
+++ app/Http/Controllers/GincanaController.php
@@ -10,90 +10,41 @@ class GincanaController extends Controller
 {
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * Pagina de gincanas.
      */
     public function index()
     {
-        $gincana = Gincana::with('autor', 'puntos')->get();
-       return view('gynkana', compact(['gincana']));
+        // $gincanas = Gincana::with('autor', 'puntos')->get();
+        return view('gincana.lista');
     }
 
-    public function listar(Request $request) {
+    /**
+     * Devolver lista de gincanas según un filtro
+     */
+    public function fitrar(Request $request) {
         $filtro = $request->input('filtro');
-if (!$filtro==''){
-    $resu = Gincana::where('nombre', 'like', '%' . $filtro . '%')->get();
-    return response()->json($resu);
-}else{
-//    $resu = Gincana::join('usuario', 'usuario.id', '=', 'gincana.user_id')
-//        ->join('punto_gincana', 'punto_gincana.id', '=', 'gincana.punto_gincana_id')
-//        ->where('user_id', 2)
-//        ->select('usuario.nombre_user', 'punto_gincana.id')
-//        ->get();
-    $resu = Gincana::with('autor', 'puntos')->where(['user_id' => 2])->get();
+        $propias = $request->input('propias');
 
-    return $resu;
-}
+        if ($propias == 'true') {
+            $resu = Gincana::with('autor', 'puntos')->where(['user_id' => auth()->user()->id])->get();
+        } else {
+            $resu = Gincana::with('autor', 'puntos')->where('nombre', 'like', $filtro.'%')->get();
+        }
+
+        return $resu;
+    }
 
 
+    public function find($id)
+    {
+        $gincana = Gincana::with('autor', 'salas', 'puntos')->find($id);
+
+        // return 
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+     * Esta función parece que no se usa para nada??
      */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Gincana  $gincana
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Gincana $gincana)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Gincana  $gincana
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Gincana $gincana)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Gincana  $gincana
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Gincana $gincana)
-    {
-        //
-    }
-
     public function getGincana()
     {
         $restaurantes = Gincana::with(['gincana.autor', 'tags'])->get();
---
+++ resources/views/gincana/lista.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@push('head')
+    <link rel="stylesheet" href="{{asset('css/gincana.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/menuPrincipal.css') }}">
+    <script type="text/javascript" src="{{asset('../resources/js/gincana.js')}}" defer></script>
+@endpush
+
+@section('content')
+    {{-- Formularios escondidos para JS --}}
+    <form action="{{ route('gincana.find', '') }}" method="get" id="form-gincanas-find"></form>
+
+    {{-- Menu Principal --}}
+    <div id="menu-principal" style="transform: translateX(0)">
+        <div id="menu-principal-navbar">
+            <i class="fa-solid fa-user"></i>
+            @auth
+                <p id="menu-principal-username">{{ auth()->user()->name }}</p>
+                {{-- <a href="{{ route('auth.logout') }}" style="color: black;"><i class="fa-solid fa-right-from-bracket"></i></a> --}}
+            @else
+                <p id="menu-principal-username">Guest</p>
+                <a href="{{ route('auth.LoginRegistrar') }}" style="color: black;"><i class="fa-solid fa-right-to-bracket"></i></a>
+            @endauth
+        </div>
+        <div id="menu-principal-contenidos">
+            <div id="menu-principal-header">GYMKHANAS</div>
+            <form action="{{ route('gincana.filtrar') }}" method="POST" id="menu-gincanas-filtros">
+                @csrf
+                <input type="text" name="buscar" id="menu-gincanas-filtro-nombre">
+                <i class="fa-regular fa-user" id="gincana-propia-toggle"></i>
+            </form>
+            <div id="menu-gincanas" class="menu-principal-botones">
+                <button class="boton-menu-principal" id="menu-principal-boton-atras"><i class="fa-solid fa-chevron-left"></i><a href="{{ route('home') }}">Atrás</a></button>
+
+            </div>
+        </div>
+    </div>
+@endsection
+
